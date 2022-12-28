010cf51a7bd81e6194e4f3f57261d0fae5ba8e6f
Nikolay Belokonenko <belokonenkons@gmail.com>
2022-12-28 18:08:28 +0200

    User crud

---
+++ CompetitionFront/ZionetCompetition/Data/ZionetCompetitionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ZionetCompetition.Models;
+
+namespace ZionetCompetition.Data
+{
+    public class ZionetCompetitionContext : DbContext
+    {
+        public ZionetCompetitionContext (DbContextOptions<ZionetCompetitionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ZionetCompetition.Models.Users> Users { get; set; } = default!;
+    }
+}
--- CompetitionFront/ZionetCompetition/Program.cs
+++ CompetitionFront/ZionetCompetition/Program.cs
@@ -2,11 +2,15 @@ using Auth0.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using ZionetCompetition.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ZionetCompetitionContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ZionetCompetitionContext") ?? throw new InvalidOperationException("Connection string 'ZionetCompetitionContext' not found.")));
 builder.Services.AddServerSideBlazor();
 //builder.Services.AddSingleton<WeatherForecastService>();
 
