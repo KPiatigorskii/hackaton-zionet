0892187007c760a9d0468cba012ca63a93a7ffa3
KPiatigorskii <k.piatigorskii@gmail.com>
2023-01-09 23:19:04 +0200

    add server listener

--- twitterEngine/app.ts
+++ twitterEngine/app.ts
@@ -1,3 +1,5 @@
+import http from 'http';
+import express, { Express } from 'express';
 import { TwitterApi } from 'twitter-api-v2';
 
 // Instantiate with desired auth type (here's Bearer v2 auth)
@@ -11,5 +13,43 @@ const twitterClient = new TwitterApi({
 });
 
 
+const router: Express = express();
+
+/** Parse the request */
+router.use(express.urlencoded({ extended: false }));
+/** Takes care of JSON data */
+router.use(express.json());
+
+/** RULES OF OUR API */
+router.use((req, res, next) => {
+    // set the CORS policy
+    res.header('Access-Control-Allow-Origin', '*');
+    // set the CORS headers
+    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+    // set the CORS method headers
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// /** Routes */
 
 
+/** Error handling */
+router.use((req, res, next) => {
+    const error = new Error('not found');
+    return res.status(404).json({
+        message: error.message
+    });
+});
+
+/** Server */
+const httpServer = http.createServer(router);
+const PORT: any = process.env.PORT ?? 6978;
+httpServer.listen(
+    PORT, 
+    () => {
+        console.log(`The server is running on port ${PORT}`)
+    });
