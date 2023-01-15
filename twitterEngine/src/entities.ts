600c0d4156fa647043aa6352ac1d59c071720f98
KPiatigorskii <k.piatigorskii@gmail.com>
2023-01-15 10:55:21 +0200

    add user interface

--- twitterEngine/src/entities.ts
+++ twitterEngine/src/entities.ts
@@ -12,3 +12,33 @@ export interface Team {
     name: string,
     memberArray: Array<TeamMember>
 }
+
+export interface Status {
+    Id : number,
+    Title : string,
+    StatusId : number
+}
+
+export interface Role {
+    Id : number,
+    Title : string,
+    StatusId : string,
+    Status : Status
+}
+
+export interface User {
+        Id : number,
+        Email : string,
+        Github : string,
+        FirstName: string,
+        LastName : string,
+        Login : string,
+        RoleId : number,
+        CreateDate : Date,
+        UpdateDate : Date,
+        CreateUserId : number,
+        UpdateUserId : number,
+        StatusId : number,
+        Role: Role,
+        Status: Status
+    }
