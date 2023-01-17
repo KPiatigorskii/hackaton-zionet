2c19c695def060778a37cd875228e8ab24c730af
Nikolay Belokonenko <belokonenkons@gmail.com>
2023-01-17 22:27:33 +0200

    Create EventInfo Page

--- CompetitionFront/ZionetCompetition/Models/User.cs
+++ CompetitionFront/ZionetCompetition/Models/User.cs
@@ -1,21 +1,28 @@
-﻿namespace ZionetCompetition.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZionetCompetition.Models
 {
     public class User
     {
         public int Id { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = null!;
 
         public string? Github { get; set; }
 
+        [Required]
         public string FirstName { get; set; } = null!;
 
+        [Required]
         public string LastName { get; set; } = null!;
 
         public string Login { get; set; } = null!;
 
         //public string Password { get; set; } = null!;
 
+        [Required]
         public int RoleId { get; set; }
 
         public string? TwitterUserId { get; set; }
