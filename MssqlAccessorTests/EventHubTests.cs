065736e4c1392238594210fecd1df41d8c2376b0
KPiatigorskii <k.piatigorskii@gmail.com>
2023-08-17 22:09:47 +0300

    commit for ci

---
+++ MssqlAccessorTests/EventHubTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MsSqlAccessorTests
+{
+    public class EventHubTests
+    {
+        [Fact]
+        public async Task CanJoinEventHub()
+        {
+                  
+        }
+
+        [Fact]
+        public async Task CanLeaveEventHub()
+        {
+                    
+        }
+
+        // Add more test methods for other hub methods
+    }
+}
