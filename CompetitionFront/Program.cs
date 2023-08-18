948ff6f81b2449bdaf521cbd6105bea2aa48cade
KPiatigorskii <k.piatigorskii@gmail.com>
2023-08-18 18:11:29 +0300

    add needs

--- CompetitionFront/Program.cs
+++ CompetitionFront/Program.cs
@@ -258,11 +258,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    //app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
-
+builder.WebHost.UseStaticWebAssets();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSession();
